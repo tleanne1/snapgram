75902c4921b554b2505fbb95541daaa0750504ed
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-12-25 00:08:36 -0600

    created signupform

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
-import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import { Home } from "./_root/pages";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
   return (
@@ -22,6 +23,8 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   );
 };
